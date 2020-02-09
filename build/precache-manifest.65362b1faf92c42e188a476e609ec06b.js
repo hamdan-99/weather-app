d34cf77d6751e7943be32fdb507182777e54165e
strik <strike.lier@hotmail.com>
2020-02-09 12:21:03 +0100

    yarn created

---
+++ build/precache-manifest.65362b1faf92c42e188a476e609ec06b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0d82bafa372ed2ccc3b137d7c80cee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0905beee63ab939cb68e",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ca600489a7af587b2d8e",
+    "url": "/static/css/main.4263c196.chunk.css"
+  },
+  {
+    "revision": "0905beee63ab939cb68e",
+    "url": "/static/js/2.25bac13b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.25bac13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca600489a7af587b2d8e",
+    "url": "/static/js/main.87c87b23.chunk.js"
+  },
+  {
+    "revision": "89b89fff14a191b19919",
+    "url": "/static/js/runtime-main.1bdcfc2d.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  }
+]);
